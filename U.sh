9bf26804b7cf231ccbf851f2c4483437a770d46f
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-02-12 09:33:33 -0500

    01-Install-on-ubuntu-server.txt 02-Alphabetical index of directives.txt

--- U.sh
+++ U.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git add .
-git commit -m " 01-Install-on-ubuntu-server.txt "
+git commit -m " 01-Install-on-ubuntu-server.txt
+02-Alphabetical index of directives.txt "
 git push -u origin main 
