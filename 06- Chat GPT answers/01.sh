93b35b195377384ad9ec584b19fec7de709014c2
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-07-27 10:23:29 -0400

    01-Install-on-ubuntu-server.txt 02-Alphabetical index of directives.txt

--- 06- Chat GPT answers/01.sh
+++ 06- Chat GPT answers/01.sh
@@ -91,16 +91,19 @@ http://acronproject.com
 
 
 # Install Docker and Docker Compose:
-
     # Update your package list and install Docker:
 sudo apt update
 sudo apt install docker.io
-
-
-
     # tart and enable Docker to run on boot:
 sudo systemctl start docker
 sudo systemctl enable docker
+    # Install Docker Compose:
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+    # Verify the installation:
+docker --version
+docker-compose --version
+
 
 
 
